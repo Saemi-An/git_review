3421fc34fc068f322f7190735d25f6516644d300
Saemi-An <cchocolatec@naver.com>
2024-05-14 23:21:45 +0900

    The first commit

---
+++ right_star.py
@@ -0,0 +1,21 @@
+
+
+n = int(input('Please enter any single number: '))   # n = 5
+
+
+for i in range(1,n+1):   # i = 1,2,3,4,5
+    print(' '*(n-i),end='')
+    print('*'*(2*i-1))
+
+
+
+#     *
+#    ***
+#   *****
+#  *******
+# *********
+
+
+# 별은 홀수 1, 3, 5, 7, 9
+
+# 공백은 4, 3, 2, 1, 0
